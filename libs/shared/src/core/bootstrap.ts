977d0ea1612d220e1791b4cced6b0efe138da28b
ahmedyousry56 <ahmedyousry098@gmail.com>
2026-06-22 18:17:09 +0300

    refactor: standardize application bootstrapping

---
+++ libs/shared/src/core/bootstrap.ts
@@ -0,0 +1,16 @@
+import { Logger, Type } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppConfigService } from "../lib/config";
+
+export async function bootstrap<T = any>(AppModule: Type<T>) {
+    const app = await NestFactory.create(AppModule);
+    const appConfigService = app.get(AppConfigService)
+    const globalPrefix = appConfigService.app.prefix
+    app.setGlobalPrefix(globalPrefix);
+    const port = appConfigService.app.port
+    await app.listen(port);
+    Logger.log(
+        `Application is running on: ${await app.getUrl()}`,
+        'Bootstrap'
+    );
+}
